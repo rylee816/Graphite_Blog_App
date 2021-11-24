849d35eaf350f68605ff9870f16fb39ebb211c48
rylee816 <65700028+rylee816@users.noreply.github.com>
2021-11-24 11:49:42 -0500

    Correct recent post widget title

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,7 +20,7 @@ const PostWidget = ({ categories, slug }) => {
     return (
         <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4">
-                {slug ? 'Related Posts' : 'RecentPosts'}
+                {slug ? 'Related Posts' : 'Recent Posts'}
             </h3>
             {relatedPosts.map(post => (
                 <div key={post.title} className="flex items-center w-full mb-4">
