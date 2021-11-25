79031ac619f7f2d6d1c30eaed304488e44efb6d6
rylee816 <65700028+rylee816@users.noreply.github.com>
2021-11-25 08:14:27 -0500

    change margin on Post Detail main container to better fit mobile devices

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -46,7 +46,7 @@ const PostDetail = ({ post }) => {
 
 
   return (
-    <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+    <div className="bg-white -mx-6 shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md mb-6">
         <img
           src={post.featuredImage.url}
