85c130a52ab688e41af20b44d826d30f73d3d5b9
rylee816 <65700028+rylee816@users.noreply.github.com>
2021-11-25 08:21:37 -0500

    change margin on Post Card main div for mobile

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-lg -mx-3 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
         <img
           src={post.node.featuredImage.url}
